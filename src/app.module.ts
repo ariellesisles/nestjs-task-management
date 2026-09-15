1573b62fad1f261d45e07530a3f87abda34e98d7
Arielle Janine <ariellesisles@gmail.com>
2026-09-15 15:33:46 +0800

    feat(database): Add TypeORM and configure PostgreSQL connection in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,23 @@
 import { Module } from '@nestjs/common';
 import { createObserveModule } from '@nestjs/observe';
 import { TasksModule } from './tasks/tasks.module.js';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
 @Module({
   imports: [
     TasksModule,
-    // Distributed tracing, auto-correlated logs, request/job metrics, error
-    // telemetry, alarms, and more — out of the box. Sign up at https://observe.nestjs.com
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5430,
+      username: 'postgres',
+      password: 'password',
+      database: 'task_db',
+      autoLoadEntities: true,
+      synchronize:true 
+    }),
   ],
-
 })
 export class AppModule {}
