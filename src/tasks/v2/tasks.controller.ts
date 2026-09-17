949081f935d86171fc9dcddaae1eaf72a4a33f0f
Arielle Janine <ariellesisles@gmail.com>
2026-09-17 16:50:54 +0800

    feat: Update Status using repository

--- src/tasks/v2/tasks.controller.ts
+++ src/tasks/v2/tasks.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service.js';
 import { Task } from '../task.entity.js';
 import { CreateTaskDto } from '../dto/create-task.dto.js';
+import { TaskStatus } from '../task-status.enum.js';
 
 @Controller({ path: 'tasks', version: '2' })
 export class TasksController {
@@ -11,6 +12,11 @@ export class TasksController {
   async createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
     return this.tasksService.createTask(createTaskDto);
   }
+
+   @Patch('/:id/status')
+   updateTask(@Param('id') id: string, @Body('status') status: TaskStatus) {
+     return this.tasksService.updateTask(id, status);
+   }
 
   @Get()
   getTasks(): Promise<Task[]> {
@@ -23,7 +29,7 @@ export class TasksController {
   }
 
   @Delete(':id')
-  deleteTask(@Param('id') id: string): Promise<any> {
+  deleteTask(@Param('id') id: string): Promise<void> {
     return this.tasksService.deleteTask(id);
   }
 }
--- src/tasks/v2/tasks.service.ts
+++ src/tasks/v2/tasks.service.ts
@@ -1,8 +1,14 @@
-import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { TaskRepository } from '../task.repository.js';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Task } from '../task.entity.js';
 import { CreateTaskDto } from '../dto/create-task.dto.js';
+import { TaskStatus } from '../task-status.enum.js';
 
 @Injectable()
 export class TasksService {
@@ -27,6 +33,24 @@ export class TasksService {
 
   createTask(createTaskDto: CreateTaskDto): Promise<Task> {
     return this.taskRepository.createTask(createTaskDto);
+  }
+
+  async updateTask(id: string, status: TaskStatus): Promise<Task> {
+    const task = await this.getTaskById(id);
+    const normalizedStatus = status.toUpperCase();
+
+    task.status = normalizedStatus as TaskStatus;
+
+    const isValidStatus = Object.values(TaskStatus).includes(
+      normalizedStatus as TaskStatus,
+    );
+
+    if (!isValidStatus) {
+      throw new BadRequestException(`"${status}" is not a valid task status.`);
+    }
+
+    await this.taskRepository.save(task);
+    return task;
   }
 
   async deleteTask(id: string): Promise<void> {
