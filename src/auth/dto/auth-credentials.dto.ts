18d39c61ae37ee1781d177370388f07c426e0f29
Arielle Janine <ariellesisles@gmail.com>
2026-09-21 09:08:25 +0800

    feat(signup): Add validation for user and password

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,17 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/^(?=.{8,32}$)(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9]).+$/, {
+    message:
+      'Password must be 8–32 characters and contain at least one uppercase letter, one lowercase letter, one number, and one special character.',
+  })
   password: string;
 }
