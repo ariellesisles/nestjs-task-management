156d6627badf991b1cdb5a2172faa09367e13334
Arielle Janine <ariellesisles@gmail.com>
2026-09-22 16:02:08 +0800

    refactor: enhance main.ts with ClassSerializerInterceptor and improve task.entity.ts with Exclude decorator

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule, ObserveInstrument } from './app.module';
-import { ValidationPipe, VersioningType } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  ValidationPipe,
+  VersioningType,
+} from '@nestjs/common';
 
 async function bootstrap() {
+  // create new nest project using method of NestFactory.create
   const app = await NestFactory.create(AppModule, {
-    // create new nest project using method of NestFactory.create
     instrument: ObserveInstrument,
   });
 
-  app.useGlobalPipes(new ValidationPipe()); // init validation pipes
+  // init validation pipes
+  app.useGlobalPipes(new ValidationPipe());
 
+  // Automatically runs instanceToPlain on all HTTP responses
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  // For URI versioning
   app.enableVersioning({
     type: VersioningType.URI,
   });
