5a9279451aa2971fa67ee5431dc3b6b3f00484fd
Arielle Janine <ariellesisles@gmail.com>
2026-09-23 16:09:32 +0800

    refactor(config): add required fields to configuration validation schema

--- src/config.schema.ts
+++ src/config.schema.ts
@@ -1,6 +1,13 @@
 import Joi from 'joi';
 
 export const configValidationSchema = Joi.object({
-
-    
+  NODE_ENV: Joi.string().required(),
+  DB_TYPE: Joi.string().required(),
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.number().default(5432).required(),
+  DB_USERNAME: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_SYNC: Joi.string().required(),
+  JWT_SECRET: Joi.string().required(),
+  JWT_EXPIRES_IN: Joi.string().required(),
 });
